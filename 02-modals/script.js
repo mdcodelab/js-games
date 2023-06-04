ca2ee68356c64e9bbb4848342ce510108917032b
miha11-hash <d_mihaela@msn.com>
2023-06-04 23:47:50 +0300

    models js functionality

--- 02-modals/script.js
+++ 02-modals/script.js
@@ -1,1 +1,31 @@
 'use strict';
+let modal = document.querySelector(".modal"); // div hidden at the beginning
+let overlay = document.querySelector(".overlay"); // div hidden at the beginning
+let btnClose = document.querySelector(".close-modal"); // close button
+let buttons = document.querySelectorAll(".show-modal");
+
+//open model
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", openModal);
+});
+
+//close modal
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnClose.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+//close modal on keydown event
+document.addEventListener("keydown", (e)=> {
+    if(e.key === "Escape" && !modal.classList.contains("hidden")) {closeModal()}
+        
+})
