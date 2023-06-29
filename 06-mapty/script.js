c4695dc9f4dd8a8c4925e9dda352f9c9ea825f64
miha11-hash <d_mihaela@msn.com>
2023-06-29 13:46:16 +0300

    display the marker

--- 06-mapty/script.js
+++ 06-mapty/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = ['January', 'February', 'March',
+  'April','May','June','July','August','September','October','November','December'];
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -10,3 +10,56 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//get position
+let map;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      console.log(`https://www.google.com/maps/@${lat},${long}z?entry=ttu`);
+      const myCoords = [lat, long];
+      map = L.map('map').setView(myCoords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+``    
+//handling clicks on map
+      map.on('click', function (mapEvent) {
+        //show form
+        form.classList.remove("hidden");
+        inputDistance.focus();
+        //show marks
+        console.log(mapEvent.latlng);
+        let latitude = mapEvent.latlng.lat;
+        let longitude = mapEvent.latlng.lng;
+        console.log(latitude, longitude);
+
+        L.marker([latitude, longitude])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Could not get your position!');
+    }
+  );
+}
+
+//
+form.addEventListener("submit", (e) => {
+//display the marker
+})
